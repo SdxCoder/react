274b10388c1baccd26677f2a62ac12f332a70901
Saad <chsaadahmed93@gmail.com>
2024-12-04 10:08:08 -0700

    update: remove junk

--- 01counter/src/components/theme_switcher.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-import { ThemeContext } from "../theme/theme_provider"
-
-
-
-export default function ThemeSwitcher() {
-    const { setThemeMode, isThemeLight } = useContext(ThemeContext)
-    return <button onClick={() => setThemeMode(isThemeLight() ? 'dark' : 'light')}  >Toggle Theme to {isThemeLight() ? 'dark' : 'light'}</button>
-}
